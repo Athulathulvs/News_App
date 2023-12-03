82142e737a2bc718c9c479b9ba2772cfc794c911
Athulvs <97433492+Athulathulvs@users.noreply.github.com>
2023-12-03 19:59:54 +0530

    Update Constance.kt

--- app/src/main/java/com/athulvs987/myapplication/utils/Constance.kt
+++ app/src/main/java/com/athulvs987/myapplication/utils/Constance.kt
@@ -3,5 +3,5 @@ package com.athulvs987.myapplication.utils
 object Constance {
     val API_BASE_URL =" https://newsapi.org/"
     val NEWS_COUNTRY_SOURCE = "in"
-    val API_KEY="9b5558121eee462a90603d11fe2b149b"
+    val API_KEY="add your key!!"
 }
